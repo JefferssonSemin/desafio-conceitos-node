acc1bfb880203b628e837a56089c6b644c82fc75
JefferssonSemin <jeffesemin@hotmail.com>
2020-04-16 13:15:49 -0300

    Adicionado método de inserir likes

--- src/app.js
+++ src/app.js
@@ -21,7 +21,7 @@ app.post("/repositories", (request, response) => {
       title,
       url,
       techs,
-      like: 0
+      likes: 0
   };
 
   repositories.push(repository);
@@ -34,19 +34,22 @@ app.put("/repositories/:id", (request, response) => {
   const {title, url, techs} = request.body;
   const {id} = request.params;
 
-  const newRepository = {
-    id,
-    title,
-    url,
-    techs,
-    likes: 0
-  };
 
   const indexRepository = repositories.findIndex(repository => repository.id === id);
 
   if(indexRepository < 0){return response.status(400).json("Repository does not exist");}
 
-  repositories[indexRepository] = newRepository; 
+  const {likes} = repositories[indexRepository]; 
+
+  const newRepository = {
+    id,
+    title,
+    url,
+    techs,
+    likes
+  };
+
+  repositories[indexRepository]= newRepository;
 
   return response.json(newRepository);
 });
@@ -54,7 +57,7 @@ app.put("/repositories/:id", (request, response) => {
 app.delete("/repositories/:id", (request, response) => {
   const {id} = request.params;
 
-  if(!isUuid(id)){return response.json("Id does not valid");}
+  if(!isUuid(id)){return response.status(400).json("Id does not valid");}
 
   const repository = repositories.find(repo => repo.id === id);
 
@@ -64,7 +67,21 @@ app.delete("/repositories/:id", (request, response) => {
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const {id} = request.params;
+
+  if(!isUuid(id)){ return response.status(400).json("Id does not valid");}
+
+  const currentIndexRepository = repositories.findIndex(repo => repo.id === id);
+
+  if(currentIndexRepository < 0){
+    return response.status(400).json("Repository does not exist");
+  }
+
+  const currentRepository = repositories[currentIndexRepository];
+  
+  currentRepository.likes++;
+ 
+  return response.json(currentRepository);
 });
 
 module.exports = app;
